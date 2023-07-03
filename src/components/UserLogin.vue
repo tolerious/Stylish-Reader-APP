44cbbfc45a9ec0135c6132ee35a709397c49b4da
Toly <tolerious.feng@kuka.com>
2023-07-03 14:52:29 +0800

    🚧 update code.

--- src/components/UserLogin.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <span>hello</span>
-</template>
---
+++ src/views/UserLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login-container">
+    <el-row
+      ><el-col> <el-divider content-position="center">欢迎使用燃烧英语</el-divider></el-col></el-row
+    >
+    <el-form label-width="100px">
+      <el-form-item label="用户名">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button>取消</el-button>
+        <el-button type="primary">登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<style lang="less" scoped>
+.login-container {
+  width: 500px;
+  margin: 200px auto;
+}
+</style>
