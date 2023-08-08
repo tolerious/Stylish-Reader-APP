dfc3fca852dc8fd04a898390e29f38a58525d3e5
He Long <tolerious@qq.com>
2023-08-08 20:09:51 +0800

    :whale: W

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="login-container">
     <div class="inner-login-container">
-      <el-row><el-col :span="24"> <el-divider content-position="center">欢迎使用燃烧英语</el-divider></el-col></el-row>
+      <el-row><el-col :span="24"> <el-divider content-position="center">欢迎使用燃烧吧英语</el-divider></el-col></el-row>
       <el-row>
         <el-col :span="20">
           <el-form label-width="100px">
-            <el-form-item label="用户名">
+            <el-form-item label="Username">
               <el-input v-model="userInfo.username"></el-input>
             </el-form-item>
-            <el-form-item label="密码">
+            <el-form-item label="Password">
               <el-input v-model="userInfo.password" type="password"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button @click="register">注册</el-button>
-              <el-button type="primary" @click="login">登陆</el-button>
+              <el-button @click="register">Register</el-button>
+              <el-button type="primary" @click="login">Login</el-button>
             </el-form-item>
           </el-form>
         </el-col>
