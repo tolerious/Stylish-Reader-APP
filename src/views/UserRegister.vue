5eac30fbb8165a17f693957c708b59e88216a03d
He Long <tolerious@qq.com>
2023-08-08 20:10:33 +0800

    :construction: W

--- src/views/UserRegister.vue
+++ src/views/UserRegister.vue
@@ -5,18 +5,24 @@
             <el-row>
                 <el-col :span="20">
                     <el-form :model="userInfo" ref="formRef" :rules="rules" label-width="100px" status-icon>
-                        <el-form-item label="输入用户名" prop="username">
-                            <el-input v-model="userInfo.username" autocomplete="off"></el-input>
+                        <el-form-item label="Phone" prop="username">
+                            <el-input maxlength="11" clearable placeholder="Input phone number" v-model="userInfo.username"
+                                autocomplete="off"></el-input>
                         </el-form-item>
-                        <el-form-item label="输入密码" prop="password">
-                            <el-input v-model="userInfo.password" type="password" autocomplete="off"></el-input>
+                        <el-form-item label="Code" prop="code">
+                            <div class="code-container">
+                                <el-input maxlength="4" clearable placeholder="Sms code" style="width:70%;"
+                                    v-model="userInfo.code" autocomplete="off"></el-input>
+                                <el-button style="width: 25%;" type="primary">Send</el-button>
+                            </div>
                         </el-form-item>
-                        <el-form-item label="再次输入密码" prop="repeatPassword">
-                            <el-input v-model="userInfo.repeatPassword" type="password" autocomplete="off"></el-input>
+                        <el-form-item label="Password" prop="password">
+                            <el-input clearable v-model="userInfo.password" type="password" autocomplete="off"></el-input>
                         </el-form-item>
+
                         <el-form-item>
                             <el-button @click="goBack">Back</el-button>
-                            <el-button type="primary" @click="register">Register</el-button>
+                            <el-button type="primary" @click="register(formRef)">Register</el-button>
                         </el-form-item>
                     </el-form>
                 </el-col>
@@ -28,17 +34,27 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { request } from '@/utils/service'
-import { ElMessage, ElNotification } from 'element-plus';
+import { ElMessage, ElNotification, type FormInstance } from 'element-plus';
 
 
 // #region function
 const validateUserName = (rule: any, vc: any, callback: any) => {
-    if (!vc) {
-        callback(new Error('Please input username'))
+    if (!vc || vc.trim().length != 11) {
+        callback(new Error('Username invalid'))
     } else {
         callback()
     }
 }
+
+function validateCode(rule: any, value: any, callback: any) {
+    let r = /^[0-9]*$/
+    if (r && r.test(value) && value.length == 4) {
+        callback()
+    } else {
+        callback(new Error('Please enter code'))
+    }
+}
+
 function validatePassword(rule: any, value: any, callback: any) {
     if (!value) {
         callback(new Error('Please input password'))
@@ -46,48 +62,28 @@ function validatePassword(rule: any, value: any, callback: any) {
         callback()
     }
 }
-function validateCheckPassword(rule: any, value: any, callback: any) {
-    if (!value) { callback(new Error('Please input password')) } else {
-        if (userInfo.value.password != value) {
-            callback(new Error("Two inputs won't match"))
-        } else {
-            callback()
-        }
-    }
-}
-const register = async () => {
-    const info = await request({
-        url: '/user/create', method: 'post', data: {
-            username: userInfo.value.username,
-            password: userInfo.value.password
-        }
+const register = async (formEl: FormInstance | undefined) => {
+    formEl?.validate(valid => {
+        console.log(valid);
+
     })
-    if (info.code === 200) {
-        ElMessage({ message: 'Successfully!', type: 'success', duration: 1200 })
-        router.push('/')
-    } else {
-        ElMessage({ message: info.msg, duration: 1200, type: 'error' })
-    }
+    // const info = await request({
+    //     url: '/user/create', method: 'post', data: {
+    //         username: userInfo.value.username,
+    //         password: userInfo.value.password
+    //     }
+    // })
+    // if (info.code === 200) {
+    //     ElMessage({ message: 'Successfully!', type: 'success', duration: 1200 })
+    //     router.push('/')
+    // } else {
+    //     ElMessage({ message: info.msg, duration: 1200, type: 'error' })
+    // }
 }
 const goBack = () => {
     router.go(-1)
 }
-async function login() {
-    const user = await request({ url: '/logic/login', method: 'post', data: userInfo.value }) as any
-    localStorage.setItem('token', user.data.token)
-    if (user.data.token) {
-        ElNotification({
-            message: 'Login Successfully', type: 'success', duration: 1200
-        })
-        setTimeout(() => {
-            router.push('/')
-        }, 1500);
-    } else {
-        ElNotification({
-            message: 'Login failed', type: 'fail', duration: 1200
-        })
-    }
-}
+
 // #endregion
 
 // #region variable
@@ -95,12 +91,12 @@ const formRef = ref()
 const rules = ref({
     username: [{ validator: validateUserName, trigger: 'blur' }],
     password: [{ validator: validatePassword, trigger: 'blur' }],
-    repeatPassword: [{ validator: validateCheckPassword, trigger: 'blur' }]
+    code: [{ validator: validateCode, trigger: 'blur' }]
 })
 let userInfo = ref({
     username: '',
     password: '',
-    repeatPassword: ''
+    code: '',
 })
 const router = useRouter()
 // #endregion
@@ -122,6 +118,14 @@ const router = useRouter()
         right: 0;
         margin: auto;
         width: 100%;
+
+        .code-container {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+        }
     }
 }
 </style>
