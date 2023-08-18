e7ce56d8f5455581d03cf62ecc017f0cce9d08cf
toly <tolerious@qq.com>
2023-08-18 09:57:27 +0800

    🚧 W

--- src/views/Sharing.vue
+++ src/views/Sharing.vue
@@ -65,20 +65,20 @@ onMounted(async () => {
   await getGroupDetail()
 
 
-  // html2canvas(document.querySelector("#app")).then(canvas => {
-  //   showDom.value = false
-  //   document.getElementById('sharing-pic-container')!.appendChild(canvas);
-  //   let canvas1 = document.querySelector('canvas')
-  //   let dataURL = canvas1.toDataURL()
-  //   // var newTab = window.open('about:blank', 'image from canvas');
-  //   // newTab.document.write("<img src='" + dataURL + "' alt='from canvas'/>");
-  //   var img = new Image();
-  //   img.src = dataURL;
-  //   imgBase.value = dataURL
-  //   img.style.height = '99vh'
-  //   document.getElementById('sharing-pic-container')!.appendChild(img);
-  //   canvas1.style.display = 'none'
-  // });
+  html2canvas(document.querySelector("#app")).then(canvas => {
+    showDom.value = false
+    document.getElementById('sharing-pic-container')!.appendChild(canvas);
+    let canvas1 = document.querySelector('canvas')
+    let dataURL = canvas1.toDataURL()
+    // var newTab = window.open('about:blank', 'image from canvas');
+    // newTab.document.write("<img src='" + dataURL + "' alt='from canvas'/>");
+    var img = new Image();
+    img.src = dataURL;
+    imgBase.value = dataURL
+    img.style.height = '99vh'
+    document.getElementById('sharing-pic-container')!.appendChild(img);
+    canvas1.style.display = 'none'
+  });
 
 })
 
