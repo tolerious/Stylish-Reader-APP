7038aef6e2ef49985cdcc06c641f06441eed4606
toly <tolerious@qq.com>
2023-08-22 15:40:45 +0800

    🚧 W

--- src/views/GroupReviewDetail.vue
+++ src/views/GroupReviewDetail.vue
@@ -3,54 +3,35 @@
   <div class="group-review-detail">
     <div class="group-review-detail-inner">
       <div class="group-review-detail-inner-block">
-        <div class="title-container">Video</div>
-        <div class="content-container">
+        <div class="title-container">
+          <h3>Video</h3>
+        </div>
+        <div class="content-container" v-for="video in videoList">
           <video controls>
-            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/webm" />
+            <source :src="video" type="video/webm" />
           </video>
         </div>
       </div>
       <div class="group-review-detail-inner-block">
-        <div class="title-container">Audio</div>
-        <div class="content-container">
-          <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
+        <div class="title-container">
+          <h3>Audio</h3>
+        </div>
+        <div class="content-container" v-for="audio in audioList">
+          <audio controls :src="audio">
             <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a>
           </audio>
         </div>
       </div>
       <div class="group-review-detail-inner-block">
-        <div class="title-container">Article</div>
-        <div class="content-container">
-          <p>
-            Visitors check out a virtual power plant system during the World Artificial Intelligence Conference in
-            Shanghai in August 2019. [GAO YUWEN/FOR CHINA DAILY]
-
-            Greater efforts are needed to advance the technologies of market entities such as solar and wind energy
-            generators, electricity storage facilities and virtual power plants to accelerate their transition into a new
-            power system, experts said.
-
-            Meanwhile, mechanisms that allow electricity users to take responsibility for their carbon emissions should
-            also be introduced, they said.
-
-            Compared to traditional methods, the new system envisages a move away from reliance on fossil fuels to one
-            based on new energy sources to generate electricity.
-
-            It involves gradually increasing the proportion of renewable energy, such as solar and wind power, in
-            electricity generation.
-
-            At the same time, an upgrade to the transmission grid will ensure that the new power system will efficiently
-            harness new energy sources and facilitate a smoother integration of various power loads, as well as enhance
-            electrification use, the experts said, adding that these are essential to achieving China's green goals.
-
-            Zhang Jieqing, China program director of the Natural Resources Defense Council, said the current power system
-            is hampered by the large-scale integration of power generated from renewable energy sources and extreme
-            weather.
-
-            "The evolving supply-demand dynamic is making power system operations more complex, with issues arising from
-            intermittent new energy sources, insufficient system flexibility, and heightened difficulties in load control.
-            Urgent low-carbon transformation of the power system is imperative to combat climate change," Zhang said.
-          </p>
+        <div class="title-container">
+          <h3>Article</h3>
         </div>
+        <div class="content-container">
+          <iframe class="iframe-container" src="http://example.com" frameborder="0"></iframe>
+        </div>
+      </div>
+      <div class="review-words-btn-group">
+        <el-button type="primary" @click="handleWordReview">单词复习</el-button>
       </div>
     </div>
   </div>
@@ -59,7 +40,7 @@
 <script setup lang="ts">
 import { ElNotification } from 'element-plus';
 import { request } from '@/utils/service';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 
@@ -77,6 +58,9 @@ onMounted(() => {
 })
 
 // #region function
+function handleWordReview() {
+  router.push('/recite/normal/?sharedGroupID='+groupID.value)
+}
 function goBack() { router.go(-1) }
 
 async function getGroupDetail() {
@@ -85,6 +69,14 @@ async function getGroupDetail() {
   groupDetail.value = info.data
 }
 // #endregion
+
+const videoList = computed(() => {
+  return groupDetail.value.groupMediaUrl?.split(',').filter(item => !!item)
+})
+
+const audioList = computed(() => {
+  return groupDetail.value.groupAudioUrl?.split(',').filter(item => !!item)
+})
 
 </script>
 
@@ -98,19 +90,32 @@ async function getGroupDetail() {
       border: 1px solid #eeeeee;
       border-radius: 5px;
       margin-bottom: 15px;
-      padding: 0 8px;
+      padding: 8px 8px;
       box-sizing: border-box;
 
       .title-container {
         font-size: 15px;
-        line-height: 30px;
+        line-height: 40px;
         color: #333;
-        height: 30px;
+        height: 40px;
+
+      }
+
+      .content-container {
+        iframe {
+          width: 100%;
+        }
       }
 
       video {
         width: 100%;
       }
     }
+
+    .review-words-btn-group {
+      width: 100%;
+      text-align: center;
+    }
   }
-}</style>
+}
+</style>
