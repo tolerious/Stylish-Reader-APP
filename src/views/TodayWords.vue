a354f16196672abb28255d68d1bc4ed58cd4698b
Toly <toly.feng@gmail.com>
2025-02-02 13:24:27 +0800

    ✨ Add today's word review.

--- src/views/TodayWords.vue
+++ src/views/TodayWords.vue
@@ -1,17 +1,33 @@
 <template>
     <Header title="Words of Today" @goBack="handleGoBack"></Header>
-    <div class="p-2 grid grid-cols-5 gap-3">
-        <word-of-today-item v-for="i in 30" :key="i"></word-of-today-item>
+    <div class="p-2 grid lg:grid-cols-5 grid-cols-3 lg:gap-3 gap-2">
+        <word-of-today-item :wordText="i.en" v-for="i in todayWord.wordList" :key="i"></word-of-today-item>
     </div>
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
 import WordOfTodayItem from '@/components/WordOfTodayItem.vue';
+import { request } from '@/utils/service';
+import { onMounted, reactive } from 'vue';
 import { useRouter } from 'vue-router';
+
+const todayWord = reactive({
+    count: 0,
+    wordList: [],
+});
+
+onMounted(async () => {
+    Object.assign(todayWord, await getTodayWordCount());
+});
 
 const router = useRouter();
 function handleGoBack() {
     router.go(-1);
+}
+
+async function getTodayWordCount() {
+    const info = await request({ url: '/word/today' });
+    return info.data;
 }
 </script>
